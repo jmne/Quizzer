3aa78661c5a72c5b3da9bd1dba557ce50177aee7
Jan <jancodet@gmail.com>
2019-02-14 14:19:26 +0100

    Test Run

--- src/Quizzer/src/main/java/group/utils/Main.java
+++ src/Quizzer/src/main/java/group/utils/Main.java
@@ -4,6 +4,8 @@
 
 package group.utils;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static Control control;
@@ -12,7 +14,24 @@ public class Main {
         control = new Control();
 
         Question q = Utils.getQuestion();
-        System.out.println(q.getId());
+        System.out.println("Zufällige Frage:");
+        System.out.println("ID: " + q.getId());
+        System.out.println("Kategorie: " + q.getCategory());
+        System.out.println("Frage: " + q.getFrage());
+        System.out.println("Antwort 1: " + q.getA1());
+        System.out.println("Antwort 2: " + q.getA2());
+        System.out.println("Antwort 3: " + q.getA3());
+        System.out.println("Antwort 4: " + q.getA4());
+
+        try {
+            TimeUnit.SECONDS.sleep(7);
+
+            System.out.println("Richtige Antwort: " + q.getRichtigA());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
