c15ff76226cbea80c5f85c60e6d6e5f6c3bae03d
Jan <jancodet@gmail.com>
2019-02-27 12:55:22 +0100

    Update

--- src/Quizzer/src/main/group/utils/Main.java
+++ src/Quizzer/src/main/group/utils/Main.java
@@ -6,10 +6,9 @@ package src.main.group.utils;
 
 import src.main.group.handlers.GameState;
 
-import java.util.concurrent.TimeUnit;
-
 public class Main {
 
+    public static Benutzer user;
     public static Control control;
 
     public static void main(String[] args) {
@@ -17,25 +16,11 @@ public class Main {
         GameState.setState(GameState.MENU);
 
         control = new Control();
+        user = new Benutzer();
 
-        Question q = new Question();
-        System.out.println("Zufällige Frage:");
-        System.out.println("ID: " + q.getId());
-        System.out.println("Kategorie: " + q.getCategory());
-        System.out.println("Frage: " + q.getFrage());
-        System.out.println("Antwort 1: " + q.getA1());
-        System.out.println("Antwort 2: " + q.getA2());
-        System.out.println("Antwort 3: " + q.getA3());
-        System.out.println("Antwort 4: " + q.getA4());
 
-        try {
-            TimeUnit.SECONDS.sleep(7);
+        login l = new login();
 
-            System.out.println("Richtige Antwort ist: " + q.getRichtigA());
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
 
     }
